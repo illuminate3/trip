fb736bed43487aa10ce31460cabf58927ddba1f1
= <rpaudel58@gmail.com>
2016-08-23 15:05:34 +0545

    folder structure

--- resources/views/tours/_gallery.blade.php
+++ resources/views/tours/_gallery.blade.php
@@ -1,11 +1,12 @@
- <div class="row">
-    <div class="section-title">
-        <h3>View Gallery</h3>
-
+<div class="row">
+    <div class="slider-for">
+        @foreach($tours->galleries as $gallery)
+            <a href="{{ asset('/uploads/images/gallery/'.$gallery->image) }}" title="{{ $gallery->title }}"><img src="{{ asset('/uploads/images/gallery/th_'.$gallery->image)}}" width="75" height="75"></a>
+        @endforeach
     </div>
-    <div class="popup-gallery">
-    @foreach($tour->galleries as $gallery)
-        <a href="{{ asset('/uploads/images/gallery/'.$gallery->image) }}" title="{{ $gallery->title }}"><img src="{{ asset('/uploads/images/gallery/th_'.$gallery->image)}}" width="75" height="75"></a>
-    @endforeach
+    <div class="slider-nav">
+        @foreach($tours->galleries as $gallery)
+            <a href="{{ asset('/uploads/images/gallery/'.$gallery->image) }}" title="{{ $gallery->title }}"><img src="{{ asset('/uploads/images/gallery/th_'.$gallery->image)}}" width="75" height="75"></a>
+        @endforeach
     </div>
 </div>
--- resources/views/vehicles/_gallery.blade.php
+++ resources/views/vehicles/_gallery.blade.php
@@ -1,11 +1,12 @@
 <div class="row">
-    <div class="section-title">
-        <h3>View Gallery</h3>
-
+    <div class="slider-for">
+        @foreach($vehicle->galleries as $gallery)
+            <a href="{{ asset('/uploads/images/gallery/'.$gallery->image) }}" title="{{ $gallery->title }}"><img src="{{ asset('/uploads/images/gallery/th_'.$gallery->image)}}" width="75" height="75"></a>
+        @endforeach
     </div>
-    <div class="popup-gallery">
-    @foreach($vehicle->galleries as $gallery)
-        <a href="{{ asset('/uploads/images/gallery/'.$gallery->image) }}" title="{{ $gallery->title }}"><img src="{{ asset('/uploads/images/gallery/th_'.$gallery->image)}}" width="75" height="75"></a>
-    @endforeach
+    <div class="slider-nav">
+        @foreach($vehicle->galleries as $gallery)
+            <a href="{{ asset('/uploads/images/gallery/'.$gallery->image) }}" title="{{ $gallery->title }}"><img src="{{ asset('/uploads/images/gallery/th_'.$gallery->image)}}" width="75" height="75"></a>
+        @endforeach
     </div>
 </div>
